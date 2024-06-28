c5a61a8a2a4069a161f9672d07dc6e3b8b58ce05
Admin <admin@Admins-MacBook-Pro.local>
2024-06-28 14:18:46 -0400

    init commit

---
+++ index.js
@@ -0,0 +1,114 @@
+const {
+    Pool
+} = require('pg');
+const express = require("express");
+const {
+    body,
+    validationResult
+} = require('express-validator');
+const app = express();
+const cors = require("cors");
+const redis = require('redis');
+const config = require('./config.json');
+
+
+const publicCorsOptions = {
+    origin: "*",
+    optionsSuccessStatus: 200,
+    credentials: true,
+}
+
+app.use(cors(publicCorsOptions));
+
+app.use(express.json());
+
+const postgresPool = new Pool({
+    user: config.postgres.user,
+    host: config.postgres.host,
+    database: config.postgres.database,
+    password: config.postgres.password,
+    port: config.postgres.port,
+    max: config.postgres.max,
+});
+
+const SORT_METHODS = {
+    newest: " ORDER BY time_created DESC",
+    oldest: " ORDER BY time_created ASC",
+};
+
+
+app.post('/get-farms', [
+
+        body('page').optional().isInt({
+            min: 1
+        }).withMessage('Page must be a positive integer'),
+
+        body('limit').optional().isInt({
+            min: 1,
+            max: 100
+        }).withMessage('Limit must be a positive integer and max 100'),
+
+        body('sort').optional().isIn(Object.keys(SORT_METHODS)).withMessage('Invalid sort method'),
+
+        body('creator')
+            .optional()
+            .matches(/^[a-z1-5.]+$/).withMessage('Invalid creator format: only a-z, 1-5, and . are allowed')
+            .isLength({ min: 1, max: 12 }).withMessage('Creator length must be between 1 and 12 characters')        
+
+    ] async (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const creator = req.body.creator || "";
+    const page = req.body.page || 1;
+    const limit = req.body.limit || 100;
+    const sort = SORT_METHODS[req.body.sort] || SORT_METHODS["newest"];
+
+    let postgresClient = null;
+
+    try {
+
+        postgresClient = await postgresPool.connect();
+
+        try {
+
+            let paramCounter = 0;
+            let params = [];
+
+            let queryString = `
+              SELECT *
+              FROM tokenfarms_farms
+            `;
+
+            if(creator != ""){
+                queryString += ` WHERE creator = $${++paramCounter}`;
+                params.push(creator);
+            }
+
+            query_string += `${sort} LIMIT ${limit} OFFSET ${(limit * page) - limit}`;
+
+            const selectResult = await postgresClient.query(queryString, params);
+            res.send({farms: selectResult.rows});     
+
+        } catch (e) {
+            res.status(500).send('Server error'); 
+        }
+
+
+    } catch (e) {
+        res.status(500).send('Server error'); 
+    } finally {
+        if(postgresClient){
+            postgresClient.release();
+        }            
+    }
+
+});
+
+
+app.listen(config.express.port, () => {
+    console.log(`Token Farms API is running on ${config.express.port}`)
+})
